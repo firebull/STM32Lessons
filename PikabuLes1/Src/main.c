e80bd9cff38432b67ca77a7a651fe44cb34135c7
Nikita Bulaev <djfirebull@gmail.com>
2017-12-01 16:39:43 +0300

    Lesson 1

---
+++ PikabuLes1/Src/main.c
@@ -0,0 +1,509 @@
+/**
+  ******************************************************************************
+  * File Name          : main.c
+  * Description        : Main program body
+  ******************************************************************************
+  * This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * Copyright (c) 2017 STMicroelectronics International N.V.
+  * All rights reserved.
+  *
+  * Redistribution and use in source and binary forms, with or without
+  * modification, are permitted, provided that the following conditions are met:
+  *
+  * 1. Redistribution of source code must retain the above copyright notice,
+  *    this list of conditions and the following disclaimer.
+  * 2. Redistributions in binary form must reproduce the above copyright notice,
+  *    this list of conditions and the following disclaimer in the documentation
+  *    and/or other materials provided with the distribution.
+  * 3. Neither the name of STMicroelectronics nor the names of other
+  *    contributors to this software may be used to endorse or promote products
+  *    derived from this software without specific written permission.
+  * 4. This software, including modifications and/or derivative works of this
+  *    software, must execute solely and exclusively on microcontroller or
+  *    microprocessor devices manufactured by or for STMicroelectronics.
+  * 5. Redistribution and use of this software other than as permitted under
+  *    this license is void and will automatically terminate your rights under
+  *    this license.
+  *
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT
+  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f1xx_hal.h"
+#include "cmsis_os.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private variables ---------------------------------------------------------*/
+RTC_HandleTypeDef hrtc;
+
+osThreadId defaultTaskHandle;
+osThreadId buttonPressHandle;
+osThreadId Led1Handle;
+osThreadId Led2Handle;
+osSemaphoreId ledOnSemHandle;
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_RTC_Init(void);
+void StartDefaultTask(void const * argument);
+void StartButtonTask(void const * argument);
+void StartLed1Task(void const * argument);
+void StartLed2Task(void const * argument);
+
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+int main(void) {
+
+    /* USER CODE BEGIN 1 */
+
+    /* USER CODE END 1 */
+
+    /* MCU Configuration----------------------------------------------------------*/
+
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
+
+    /* USER CODE BEGIN Init */
+
+    /* USER CODE END Init */
+
+    /* Configure the system clock */
+    SystemClock_Config();
+
+    /* USER CODE BEGIN SysInit */
+
+    /* USER CODE END SysInit */
+
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_RTC_Init();
+
+    /* USER CODE BEGIN 2 */
+
+    /* USER CODE END 2 */
+
+    /* USER CODE BEGIN RTOS_MUTEX */
+    /* add mutexes, ... */
+    /* USER CODE END RTOS_MUTEX */
+
+    /* Create the semaphores(s) */
+    /* definition and creation of ledOnSem */
+    osSemaphoreDef(ledOnSem);
+    ledOnSemHandle = osSemaphoreCreate(osSemaphore(ledOnSem), 1);
+
+    /* USER CODE BEGIN RTOS_SEMAPHORES */
+    /* add semaphores, ... */
+    /* USER CODE END RTOS_SEMAPHORES */
+
+    /* USER CODE BEGIN RTOS_TIMERS */
+    /* start timers, add new ones, ... */
+    /* USER CODE END RTOS_TIMERS */
+
+    /* Create the thread(s) */
+    /* definition and creation of defaultTask */
+    osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
+    defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
+
+    /* definition and creation of buttonPress */
+    osThreadDef(buttonPress, StartButtonTask, osPriorityNormal, 0, 128);
+    buttonPressHandle = osThreadCreate(osThread(buttonPress), NULL);
+
+    /* definition and creation of Led1 */
+    osThreadDef(Led1, StartLed1Task, osPriorityNormal, 0, 128);
+    Led1Handle = osThreadCreate(osThread(Led1), NULL);
+
+    /* definition and creation of Led2 */
+    osThreadDef(Led2, StartLed2Task, osPriorityIdle, 0, 128);
+    Led2Handle = osThreadCreate(osThread(Led2), NULL);
+
+    /* USER CODE BEGIN RTOS_THREADS */
+    /* add threads, ... */
+    /* USER CODE END RTOS_THREADS */
+
+    /* USER CODE BEGIN RTOS_QUEUES */
+    /* add queues, ... */
+    /* USER CODE END RTOS_QUEUES */
+
+
+    /* Start scheduler */
+    osKernelStart();
+
+    /* We should never get here as control is now taken by the scheduler */
+
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
+    while (1) {
+        /* USER CODE END WHILE */
+
+        /* USER CODE BEGIN 3 */
+
+    }
+
+    /* USER CODE END 3 */
+
+}
+
+/** System Clock Configuration
+*/
+void SystemClock_Config(void) {
+
+    RCC_OscInitTypeDef RCC_OscInitStruct;
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    RCC_PeriphCLKInitTypeDef PeriphClkInit;
+
+    /**Initializes the CPU, AHB and APB busses clocks
+    */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI | RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        _Error_Handler(__FILE__, __LINE__);
+    }
+
+    /**Initializes the CPU, AHB and APB busses clocks
+    */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                                  | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+        _Error_Handler(__FILE__, __LINE__);
+    }
+
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_RTC;
+    PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
+
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
+        _Error_Handler(__FILE__, __LINE__);
+    }
+
+    /**Configure the Systick interrupt time
+    */
+    HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq() / 1000);
+
+    /**Configure the Systick
+    */
+    HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+    /* SysTick_IRQn interrupt configuration */
+    HAL_NVIC_SetPriority(SysTick_IRQn, 15, 0);
+}
+
+/* RTC init function */
+static void MX_RTC_Init(void) {
+
+    RTC_TimeTypeDef sTime;
+    RTC_DateTypeDef DateToUpdate;
+
+    /**Initialize RTC Only
+    */
+    hrtc.Instance = RTC;
+    hrtc.Init.AsynchPrediv = RTC_AUTO_1_SECOND;
+    hrtc.Init.OutPut = RTC_OUTPUTSOURCE_ALARM;
+
+    if (HAL_RTC_Init(&hrtc) != HAL_OK) {
+        _Error_Handler(__FILE__, __LINE__);
+    }
+
+    /**Initialize RTC and set the Time and Date
+    */
+    if (HAL_RTCEx_BKUPRead(&hrtc, RTC_BKP_DR1) != 0x32F2) {
+        sTime.Hours = 0x1;
+        sTime.Minutes = 0x0;
+        sTime.Seconds = 0x0;
+
+        if (HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BCD) != HAL_OK) {
+            _Error_Handler(__FILE__, __LINE__);
+        }
+
+        DateToUpdate.WeekDay = RTC_WEEKDAY_MONDAY;
+        DateToUpdate.Month = RTC_MONTH_JANUARY;
+        DateToUpdate.Date = 0x1;
+        DateToUpdate.Year = 0x0;
+
+        if (HAL_RTC_SetDate(&hrtc, &DateToUpdate, RTC_FORMAT_BCD) != HAL_OK) {
+            _Error_Handler(__FILE__, __LINE__);
+        }
+
+        HAL_RTCEx_BKUPWrite(&hrtc, RTC_BKP_DR1, 0x32F2);
+    }
+
+}
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+static void MX_GPIO_Init(void) {
+
+    GPIO_InitTypeDef GPIO_InitStruct;
+
+    /* GPIO Ports Clock Enable */
+    __HAL_RCC_GPIOD_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+    /*Configure GPIO pin Output Level */
+    HAL_GPIO_WritePin(GPIOB, LED1_Pin | LED2_Pin, GPIO_PIN_RESET);
+
+    /*Configure GPIO pin : BUTTON_Pin */
+    GPIO_InitStruct.Pin = BUTTON_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    HAL_GPIO_Init(BUTTON_GPIO_Port, &GPIO_InitStruct);
+
+    /*Configure GPIO pins : LED1_Pin LED2_Pin */
+    GPIO_InitStruct.Pin = LED1_Pin | LED2_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /* EXTI interrupt init*/
+    HAL_NVIC_SetPriority(EXTI0_IRQn, 5, 0);
+    HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/* StartDefaultTask function */
+void StartDefaultTask(void const * argument) {
+
+    /* USER CODE BEGIN 5 */
+    /*****************
+     * Задача по-умолчанию.
+     * Я предпочитаю пихать в неё всякие рутинные
+     * вещи, вроде инициальзации и сброса состояний,
+     * опроса с равными промежутками и прочее.
+     *
+     * Например, сейчас я сделал запуск задаи раз в секунду
+     *****************/
+
+    TickType_t xLastWakeTime;
+    const TickType_t xFrequency = 1000;
+    xLastWakeTime = xTaskGetTickCount();
+
+    for (;;) {
+        // Тут можно запускать какие-то действия
+
+
+        // Благодаря этой функции, задача будет
+        // уходить на новый круг в равные промежутки
+        // времени.
+        vTaskDelayUntil(&xLastWakeTime, xFrequency);
+    }
+
+    /* USER CODE END 5 */
+}
+
+/* StartButtonTask function */
+void StartButtonTask(void const * argument) {
+    /* USER CODE BEGIN StartButtonTask */
+    // Внутри этой функции мы создадим отдельную переменную
+    // для хранения состояния кнопки
+
+    uint32_t notifyState = 0x00;
+
+    for (;;) {
+        xTaskNotifyWait(0x00,              /* Не очищать никакие биты на входе в функцию*/
+                        ULONG_MAX,         /* Очищать все биты состояния на выходе из функции*/
+                        &notifyState,      /* Состояние уведомлений извне */
+                        ULONG_MAX);        /* Ожидаем нажатия кнопки до бесконечности */
+
+        if ((notifyState & BUTTON_PRESSED) == BUTTON_PRESSED) {
+            // Если кнопка нажата, то перехватим семафор
+            // и зажгём оба светодиода
+            // Ждём ограниченное время, иначе при нескольких
+            // нажатиях подряд можем зависнуть тут навсегда
+            if (xSemaphoreTake(ledOnSemHandle, 1000) == pdTRUE) {
+                HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_SET);
+                HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_SET);
+            } else {
+                // Если, вдруг, не удалось перехватить семафор,
+                // Не делаем ничего, возвращаемся в состояние ожидания.
+                // Даже если кнопка будет нажата, ничего меняться не будет!
+                // Это наихудший вариант, если вдруг нам не удастся
+                // выхватить семафор за 1000.
+                //
+                // ТАКИЕ ВЕЩИ НАДО ВСЕГДА УЧИТЫВАТЬ!!!
+            }
+
+        } else if ((notifyState & BUTTON_RELEASED) == BUTTON_RELEASED) {
+            // Если кнопка отжата, то погасим оба светодиода
+            // и отдадим семафор
+            HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_RESET);
+            HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_RESET);
+            xSemaphoreGive(ledOnSemHandle);
+        }
+
+        /**
+         * ВНИМАНИЕ!
+         *
+         * Вообще, так делать, конечно, плохо. В таком простом случае,
+         * всё будет хорошо. Но в сложных программах нельзя допускать
+         * ситуацию, когда кто-то перехватывает семафор и уходит
+         * в бесконечный сон.
+         */
+    }
+
+    /* USER CODE END StartButtonTask */
+}
+
+/* StartLed1Task function */
+void StartLed1Task(void const * argument) {
+    /* USER CODE BEGIN StartLed1Task */
+    for (;;) {
+        // Ждём до бесконечности, пока не сможем перехватить семафор
+        if (xSemaphoreTake(ledOnSemHandle, ULONG_MAX) == pdTRUE) {
+            // Когда семафор наш, зажигаем светодиод
+            HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_SET);
+
+            // Разблокирем задачу на 0,5 секунды, чтобы остальные
+            // задачи могли куртиться дальше
+            vTaskDelay(500);
+
+            // Гасим светодиод
+            HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_RESET);
+
+            // Освобождаем семафор, чтобы другие могли его взять
+            xSemaphoreGive(ledOnSemHandle);
+        }
+
+        // Разблокируем задачу на 0,5 секунды, чтобы остальные
+        // могли работать дальше и у них была возможность
+        // перехватить семафор себе
+        vTaskDelay(500);
+    }
+
+    /* USER CODE END StartLed1Task */
+}
+
+/* StartLed2Task function */
+void StartLed2Task(void const * argument) {
+    /* USER CODE BEGIN StartLed2Task */
+    for (;;) {
+        if (xSemaphoreTake(ledOnSemHandle, ULONG_MAX) == pdTRUE) {
+            HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_SET);
+            vTaskDelay(500);
+            HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_RESET);
+            xSemaphoreGive(ledOnSemHandle);
+        }
+
+        vTaskDelay(500);
+    }
+
+    /* USER CODE END StartLed2Task */
+}
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM4 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+    /* USER CODE BEGIN Callback 0 */
+
+    /* USER CODE END Callback 0 */
+    if (htim->Instance == TIM4) {
+        HAL_IncTick();
+    }
+
+    /* USER CODE BEGIN Callback 1 */
+
+    /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+void _Error_Handler(char * file, int line) {
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state */
+    while (1) {
+    }
+
+    /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef USE_FULL_ASSERT
+
+/**
+   * @brief Reports the name of the source file and the source line number
+   * where the assert_param error has occurred.
+   * @param file: pointer to the source file name
+   * @param line: assert_param error line source number
+   * @retval None
+   */
+void assert_failed(uint8_t* file, uint32_t line) {
+    /* USER CODE BEGIN 6 */
+    /* User can add his own implementation to report the file name and line number,
+      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+    /* USER CODE END 6 */
+
+}
+
+#endif
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+*/
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
